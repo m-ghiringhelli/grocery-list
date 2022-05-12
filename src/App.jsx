e60b7aafe180853855ad153b73200751f625749b
m-ghiringhelli <mghiring@gmail.com>
2022-05-12 15:55:18 -0700

    setup input and initialize reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
+import React, { useReducer, useState } from 'react';
+
+const ACTIONS = {
+
+}
+
+const initialState = [];
+
+function reducer(state, action) {
+
+}
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  const [todos, dispatch] = useReducer(reducer, initialState);
+  const [name, setName] = useState('');
+
+  return (
+    <>
+      <form>
+        <label>
+          Add to list:
+          <input 
+            type='text' 
+            value={name} 
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+      </form>
+    </>
+  );
 }
